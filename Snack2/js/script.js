3ce353be9a659b801677dc197f9bf6446e6aed87
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-12 14:28:54 +0100

    completato snack 2

--- Snack2/js/script.js
+++ Snack2/js/script.js
@@ -3,3 +3,24 @@
 // prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
+// Inizializzo i due array
+const firstArray = [`a`, `b`, `c`];
+const secondArray = [1, 2, 3];
+
+// Funzione con i due array
+function arrayFunction(firstArray, secondArray) {
+    const array1 = firstArray.length;
+    const finalArray = [];
+
+// Ciclo for per inserire gli elementi nei due array alternati
+    for(let i = 0; i < array1; i++) {
+        finalArray.push(firstArray[i]);
+        finalArray.push(secondArray[i]);
+    }
+
+    return finalArray;
+}
+
+// Costante con i due array fusi tra loro
+const fusedArray = arrayFunction(firstArray, secondArray);
+console.log(fusedArray);
