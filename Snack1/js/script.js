63950d20ccdc556113775cc749f42f0fc955b0fb
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-12 14:02:10 +0100

    completato lo snack 1

--- Snack1/js/script.js
+++ Snack1/js/script.js
@@ -1,1 +1,13 @@
 // Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+
+// Chiedo all'utente di inserire una parola
+const stringToReverse = prompt("Inserisci una parola che vuoi invertire");
+
+// Funzione che inverte la parola inserita
+function reverseString(string){
+    return string.split(``).reverse().join(``);
+}
+
+// Costante che riceve la parola invertita
+const reversedString = reverseString(stringToReverse);
+console.log(reversedString);
