e0a0ebccd15644cc03c1e1a90270a2b9ec53f029
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-12 14:48:25 +0100

    completato snack 3

--- Snack3/js/script.js
+++ Snack3/js/script.js
@@ -6,3 +6,22 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “posizioneMin” e “posizioneMax”
 // Nel nostro caso, se la chiamassimo così tagliaArray(arrayNumeri, 2, 4) ci restituirebbe un array così composto:
 // [2, 3, 4]
+
+// Inizializzato array 
+const arrayNumbers = [0, 1, 2, 3, 4, 5, 6];
+
+// Funzione con tre argomenti
+function cutArray(array, min, max){
+    let newArray = [];
+
+// Ciclo che ritornerà i valori compresi tra min e max nella newArray
+    for (let i = min; i <= max; i++) {
+        newArray.push(array[i]);
+    }
+
+    return newArray;
+}
+
+const finalArray = cutArray(arrayNumbers, 3, 6);
+console.log(finalArray);
+
